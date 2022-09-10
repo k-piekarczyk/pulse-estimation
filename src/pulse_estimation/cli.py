328de2d3a99b6900a30c28b06de60a9ed622641a
Krzysztof Piekarczyk <krzysztof@piekarczyk.eu>
2022-09-10 03:56:50 +0200

    wip

--- src/pulse_estimation/cli.py
+++ src/pulse_estimation/cli.py
@@ -12,6 +12,7 @@ selection = [
     {"video": "./resources/video/my/face_webcam_uncontrolled.mp4", "heart_rate": 80 / 60.0},
     {"video": "./resources/video/my/face_controlled_not_diffused.mov", "heart_rate": 68.5 / 60.0},
     {"video": "./resources/video/my/face_controlled_diffused.mov", "heart_rate": 83.5 / 60.0},
+    {"video": "./resources/video/my/face_outdoors_90bpm.mp4", "heart_rate": 90 / 60.0},
     {"video": "./resources/video/other/face.mp4", "heart_rate": 54.0 / 60.0},
 ]
 
@@ -29,7 +30,7 @@ def run_for_every_target(method: Callable[..., Optional[float]], args: tuple, kw
 
         print(f"Current target video: {target_video}")
 
-        result = method(*(target_video, *args), **{"acc_hr":target_heart_rate, **kwargs})
+        result = method(*(target_video, *args), **{"acc_hr": target_heart_rate, **kwargs})
 
         if result is not None and target_heart_rate is not None:
             target_hr = target_heart_rate * 60
@@ -45,10 +46,11 @@ def run_for_every_target(method: Callable[..., Optional[float]], args: tuple, kw
 
 def main():
     run_for_every_target(
-        method=fir_filtered_RG_ICA, 
+        method=fir_filtered_RG_ICA,
         args=(face_cascade_file, hr_low, hr_high, min_YCrCb, max_YCrCb),
-        kwargs={"display_face_selection": False, "plot": False}
+        kwargs={"display_face_selection": False, "plot": False},
     )
+
 
 # naive_ICA(target[0], face_cascade_file, hr_low, hr_high, min_YCrCb, max_YCrCb, acc_hr=target[1])
 # naive_PCA(target[0], face_cascade_file, hr_low, hr_high, min_YCrCb, max_YCrCb)
